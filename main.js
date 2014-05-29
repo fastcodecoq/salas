5c32c4b3dd3f8b93461b4c5dda7b1f24423c3dd4
Javier Gomez <info@gomosoft.com>
2014-05-29 04:51:33 -0500

    readme

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
-var app = angular.module("salas", []);
+
+
+            numeral.language('es', {
+              delimiters: {
+              thousands: '.',
+              decimal: '.'
+              },
+              abbreviations: {
+              thousand: 'k',
+              million: 'm',
+              billion: 'b',
+              trillion: 't'
+              },
+              ordinal : function (number) {
+              return number === 1 ? 'er' : 'ème';
+              },
+              currency: {
+              symbol: '$'
+              }
+              });
+              
+              numeral.language('es');
+
+
+
+             var app = angular.module("salas", [])
+             .run(function($rootScope) {
+                $rootScope.dpto = {};
+                $rootScope.mcpio = {};
+              });
+
+             new login(app);
+             new registro(app);
+             new sala(app);
+             new router(app);
